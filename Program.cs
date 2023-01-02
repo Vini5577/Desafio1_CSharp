ea00c9550d35175a26edef131d7282cf7b078931
Vini5577 <ugopessoa@gmail.com>
2023-01-02 09:34:14 -0300

    primeiro desafio dio concluido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class DIO {
+        
+static void Main(string[] args){
+
+        int n = int.Parse(Console.ReadLine());
+        int p = 1, s = 0;
+        while (n > 0){
+             int l = n % 10;
+
+            p *= l;
+            s += l;
+            n /= 10;
+
+           }
+
+        Console.WriteLine("Valor de p(produto):");
+        Console.WriteLine(p);
+        Console.WriteLine("Valor de s(soma): ");
+        Console.WriteLine(s);
+        Console.WriteLine(p-s);
+    }
+}
